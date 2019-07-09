700e825b2aa5297ec39df44572bef930f7b203ae
mt <mt@qq.com>
2019-07-09 20:05:33 +0800

    增加APP_key认证

--- apps/permission/middleware.py
+++ apps/permission/middleware.py
@@ -13,19 +13,31 @@ class TokenMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.path != "/docs/":
             try:
-                Authorization = request.headers["Authorization"]
-                if Authorization:
-                    headers = {"Authorization": Authorization}
-                    r = requests.get("http://101.200.61.189:52131/userInfo/", headers=headers)
-                    if r.status_code == 200:
-                        request.user = r.json()
-                        # setattr(request, 'username', username)
+                # 子系统之间的验证
+                app_key = request.headers["app-key"]
+                app_name = request.headers["app-name"]
+                if app_name and app_key:
+                    data = {'app_key': app_key, 'app_name': app_name}
+                    r = requests.post("http://101.200.61.189:8888/authper/", data=data)
+                    if r.json()['status'] == 1:
+                        pass
                     else:
-                        content = {"detail": "Authentication credentials were not provided."}
-                        return HttpResponseAuthenticationFailed(json.dumps(content))
+                        content = r.text
+                        return HttpResponseAuthenticationFailed(content)
+                else:
+                    # 去sso认证用户是否存在
+                    Authorization = request.headers["Authorization"]
+                    if Authorization:
+                        headers = {"Authorization": Authorization}
+                        r = requests.get("http://101.200.61.189:52131/userInfo/", headers=headers)
+                        if r.status_code == 200:
+                            request.user = r.json()
+                        else:
+                            content = {"detail": "User verification does not pass."}
+                            return HttpResponseAuthenticationFailed(json.dumps(content))
             except:
-                pass
-                # content = {"detail": "Authentication credentials were not provided."}
-                # return HttpResponseAuthenticationFailed(json.dumps(content))
+                # pass
+                content = {"detail": "Authentication credentials were not provided."}
+                return HttpResponseAuthenticationFailed(json.dumps(content))
 
 
